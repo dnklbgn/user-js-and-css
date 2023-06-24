fd0d197535ed38b3b492524086dd07d5bd104ea5
nkuzmin <cool.baklazhan@gmail.com>
2023-06-25 00:08:02 +0300

    Исправлена проблема с отсутствием обоев в некоторых чатах

--- amo/chats_wallpaper.user.js
+++ amo/chats_wallpaper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        amo messenger chats wallpaper
 // @namespace   https://github.com/dnklhtbgn/js-user-scripts
-// @version     0.1
+// @version     0.2
 // @description Добавляем возможность ставить обои для чатов в мессенджере amo
 // @match       *://web.amo.tm/*
 // @grant       GM_setValue
@@ -24,7 +24,7 @@ function GM_addStyle(css) {
 
 function setWallpaper(wallpaper_path) {
     GM_addStyle(
-        '#app > div:nth-child(3) ._scrollbar.baron__clipper.baron {' +
+        '#app > div:nth-child(3) ._scrollbar.baron__clipper {' +
         'background-image: url(' + wallpaper_path + ');' +
         'background-repeat: no-repeat;' +
         'background-size: cover;' +
