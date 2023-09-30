76eca5b9f2beee467f1f377668ef6f3cf5325b0d
nkuzmin <nkuzmin@team.amocrm.com>
2023-09-30 21:56:31 +0300

    Куча всяких нужных и ненужных изменений
    - При поиске сообщений фон облачков больше не сливается с текстом сообщений;
    - Названия вкладок в информации о беседе стали более читаемыми;
    - Названия файлов во вкладке "Медиа" в информации о беседе стали более читаемыми;
    - Расшифровка голосовых сообщений больше не сливается с фоном;
    - Названия файлов в диалоге стали более читаемыми;
    - Статус исходящего сообщения стал более читаемым;
    - Кнопка создания нового канала получила темный фон (русы пока не придумали, как победить ящерскую границу этой кнопки и добавить к ней темные стили).

--- scripts/amo/dark_mode.user.js
+++ scripts/amo/dark_mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        amo messenger dark mode
 // @namespace   https://github.com/dnklhtbgn/js-user-scripts
-// @version     0.4.2
+// @version     0.5
 // @description Добавляем темную тему в мессенджер amo
 // @author      dnklhtbgn
 // @downloadURL https://github.com/dnklhtbgn/user-js-and-css/raw/master/scripts/amo/dark_mode.user.js
@@ -33,7 +33,7 @@ function GM_addStyle(css) {
   GM_addStyle('body, [class^="ForwardModal_divider__"] { background-color: #284860; }');
   GM_addStyle('#app > div:first-child { background-color: #07131d; }');
   GM_addStyle(
-    '[class^="Sidebar_container"], [class^="ConversationHeader_header"], [class^="Tab_tab_content_wrapper"], [class^="style_search_container"] label:first-child, [class^="RegularMessage_message__"], [class^="MainInput_container__"], [class^="ForwardModal_header__"], [class^="ForwardModal_controls__"], [class^="style_checkmark_border__"], [class^="MainInput_container__"] ._scrollbar.baron__clipper, [class^="ConversationRoute_compose__"] {' +
+    '[class^="Sidebar_container"], [class^="ConversationHeader_header"], [class^="Tab_tab_content_wrapper"], [class^="style_search_container"] label:first-child, [class^="RegularMessage_message__"], [class^="MainInput_container__"], [class^="ForwardModal_header__"], [class^="ForwardModal_controls__"], [class^="style_checkmark_border__"], [class^="MainInput_container__"] ._scrollbar.baron__clipper, [class^="ConversationRoute_compose__"], [class*="BaseEditableAddAction_container_dark__"] {' +
     'background: #153043;' +
     '}'
   );
@@ -58,8 +58,34 @@ function GM_addStyle(css) {
     '}'
   );
   GM_addStyle('[class*="RegularMessage_is_outgoing__"] [class^="RegularMessage_message__"] { background: #264b79; }');
+  GM_addStyle('[class*="RegularMessage_is_outgoing__"][class*="RegularMessage_is_normalized__"] [class^="RegularMessage_message__"] {' +
+    'background: #507ba3;' +
+    '}'
+  );
+  GM_addStyle('[class*="RegularMessage_is_outgoing__"][class*="RegularMessage_highlighted__"] [class^="RegularMessage_message__"] {' +
+    '-webkit-animation-name: RegularMessage_select-background-outgoing;' +
+    'animation-name: RegularMessage_select-background-outgoing;' +
+    '}'
+  );
+  GM_addStyle('[class*="RegularMessage_is_normalized__"] [class^="RegularMessage_message__"] {' +
+    'background: #3f6680;' +
+    '}'
+  );
+  GM_addStyle('[class*="RegularMessage_highlighted__"] [class^="RegularMessage_message__"] {' +
+    '-webkit-animation-name: RegularMessage_select-background;' +
+    'animation-name: RegularMessage_select-background;' +
+    '}'
+  );
+  GM_addStyle('[class*="RegularMessage_is_outgoing__"] [class^="RegularMessage_message__"] [class^="MessageDeliveryStatus_container__"] {' +
+    'color: #bcbcbc;' +
+    '}'
+  );
+  GM_addStyle('[class*="MessageDeliveryStatus_status_read__"] {' +
+    'fill: #bcbcbc;' +
+    '}'
+  );
   GM_addStyle(
-    '[class^="RegularMessage_message__"], [class^="ForwardMessage_container__"], [class^="ReferenceMessage_container__"], [class^="FeedTextarea_editable__"], [class*="style_search_input__"], [class*="style_subtitle_light__"], [class^="SearchMessages_title__"], [class^="Notification_title__"], [class^="UserMentionItem_container__"], [class^="TabCaption_name__"], [class^="FolderView_container__"] [class^="style_header__"] {' +
+    '[class^="RegularMessage_message__"], [class^="ForwardMessage_container__"], [class^="ReferenceMessage_container__"], [class^="FeedTextarea_editable__"], [class*="style_search_input__"], [class*="style_subtitle_light__"], [class^="SearchMessages_title__"], [class^="Notification_title__"], [class^="UserMentionItem_container__"], [class^="TabCaption_name__"], [class^="FolderView_container__"] [class^="style_header__"], [class^="ConversationTitle_rpa_request_name_wrapper__"], [class^="style_transcription_text__"], [class^="MessageMediaItem_media__title__"] {' +
     'color: #f2f2f2;' +
     '}'
   );
@@ -82,7 +108,7 @@ function GM_addStyle(css) {
     '}'
   );
   GM_addStyle(
-    '[class*="MessageTitle_user__"], [class^="ServiceMessage_container__"] [class^="UserLink_user_link__"], [class^="ServiceMessage_container__"], [class^="ForwardModal_header__"], [class^="BaseEditableAddAction_placeholder__"], [class*="BaseEditableAddAction_container_dark__"] {' +
+    '[class*="MessageTitle_user__"], [class^="ServiceMessage_container__"] [class^="UserLink_user_link__"], [class^="ServiceMessage_container__"], [class^="ForwardModal_header__"], [class^="BaseEditableAddAction_placeholder__"], [class*="BaseEditableAddAction_container_dark__"], [class^="Document_name__"] {' +
     'color: #8092ad;' +
     '}'
   );
@@ -106,11 +132,67 @@ function GM_addStyle(css) {
     'border-color: #284860 !important;' +
     '}'
   );
-  GM_addStyle('[class^="style_tab__"] { border-right: 1px solid #284860 }');
-  GM_addStyle('[class^="style_tabs__"]:after { background: #284860 }');
+  GM_addStyle('[class^="style_tab__"] {' +
+    'border-right: 1px solid #284860;' +
+    'color: #f2f2f2;' +
+    '}'
+  );
+  GM_addStyle('[class^="style_tab__"][class*="style_selected__"] {' +
+    'color: #2a9afd;' + // Не более чем тупой запил для того, чтобы один стиль темной темы не сносил другой
+    '}'
+  );
+  GM_addStyle('[class^="style_tabs__"]:after { background: #284860; }');
   GM_addStyle('[class^="MessageItem_unread_messages_separator__"], [class*="style_date_separator_item__"] span {' +
     'background: rgba(40, 72, 96);' +
     'color: #8092ad;' +
+    '}'
+  );
+  GM_addStyle('@-webkit-keyframes RegularMessage_select-background-outgoing {' +
+    '0% {' +
+    'background: #507ba3;' +
+    '}' +
+    '50% {' +
+    'background: #8bb3cf;' +
+    '}' +
+    'to {' +
+    'background: #507ba3;' +
+    '}' +
+    '}'
+  );
+  GM_addStyle('@keyframes RegularMessage_select-background-outgoing {' +
+    '0% {' +
+    'background: #507ba3;' +
+    '}' +
+    '50% {' +
+    'background: #8bb3cf;' +
+    '}' +
+    'to {' +
+    'background: #507ba3;' +
+    '}' +
+    '}'
+  );
+  GM_addStyle('@-webkit-keyframes RegularMessage_select-background {' +
+    '0% {' +
+    'background: #3f6680;' +
+    '}' +
+    '50% {' +
+    'background: #7fa6bd;' +
+    '}' +
+    'to {' +
+    'background: #3f6680;' +
+    '}' +
+    '}'
+  );
+  GM_addStyle('@keyframes RegularMessage_select-background {' +
+    '0% {' +
+    'background: #3f6680;' +
+    '}' +
+    '50% {' +
+    'background: #7fa6bd;' +
+    '}' +
+    'to {' +
+    'background: #3f6680;' +
+    '}' +
     '}'
   );
 })();
